41aeb3da37e0a491579311edc2a1d27bd5181808
garvey <liujiawei@phone580.com>
2020-05-18 18:15:40 +0800

    feat(all): 添加url-loader

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,27 +1,63 @@
 const paths = require('./path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
+
+const sassModuleRegex = /\.module\.(css|scss|sass)$/;
 
 module.exports = {
-	entry: {
-		app: './src/index.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				// test: /\.(js|jsx)$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: 'babel-loader'
-				}
-			},
-			{
-				test: /\.(css|scss)$/,
-				use: ['style-loader', 'css-loader', 'sass-loader']
-			}
-		]
-	},
-	output: {
-		filename: '[name].bundle.js',
-		path: paths.appBuild
-	}
+  entry: {
+    app: './src/index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/, // test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: sassModuleRegex,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          // 'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|bmp)$/,
+        use: [{
+          loader: "url-loader",
+          options: {
+            esModule: false,
+            limit: 10000,
+            name: 'assets/media/[name].[hash:8].[ext]',
+          }
+        }]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(csv|tsv|xls)$/,
+        use: ['csv-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'assets/css/[name].[contenthash:8].css',
+      chunkFilename: 'assets/css/[name].[contenthash:8].chunk.css',
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: paths.appBuild
+  }
 }
--- src/a.jsx
+++ src/a.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export default function TestA() {
-	return <div>test...Component</div>
+	const img = require('./assets/img/order-pay.png')
+
+	return <div>
+		test...Component
+		<img src={img} alt=""/>
+		{/*<img src={require('./assets/img/test1.14db315c.jpg')} alt=""/>*/}
+		<div className="img-a"></div>
+	</div>
 }
